b5400692c078253d81f0bcabe25125cc08f68934
brianchen.clx <brianchen.clx@gmail.com>
2017-03-17 19:16:45 +0800

    synch test

---
+++ Java-core/src/main/java/com/java/core/thread/bank/synch/Bank.java
@@ -0,0 +1,55 @@
+package com.java.core.thread.bank.synch;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author chenlixin at 2017年3月17日 下午5:41:38
+ */
+public class Bank {
+
+    private ReentrantLock bankLock = new ReentrantLock();
+    private final double[] accounts;
+    private Condition sufficientFunds;
+
+    public Bank(int n, int initBalance) {
+        accounts = new double[n];
+        for (int index = 0; index < accounts.length; index++) {
+            accounts[index] = initBalance;
+        }
+        sufficientFunds = bankLock.newCondition();
+    }
+
+    public void unsynchTransfer(int from, int to, double amount) {
+        System.out.print(Thread.currentThread());
+        accounts[from] -= amount;
+        System.out.printf(" %10.2f from %d to %d", amount, from, to);
+        accounts[to] += amount;
+        System.out.println("Total balance:" + getTotalBalance());
+    }
+
+    public void synchTransfer(int from, int to, double amount) {
+        bankLock.lock();
+        try {
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.println("Total balance:" + getTotalBalance());
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    public double getTotalBalance() {
+        double sum = 0;
+        for (double a : accounts) {
+            sum += a;
+        }
+        return sum;
+    }
+
+    public int size() {
+        return accounts.length;
+    }
+}
---
+++ Java-core/src/main/java/com/java/core/thread/bank/synch/UnsynchTransferTest.java
@@ -0,0 +1,17 @@
+package com.java.core.thread.bank.synch;
+
+/**
+ * @author chenlixin at 2017年3月6日 下午6:21:17
+ */
+public class UnsynchTransferTest {
+
+    public static final int NNACOUNTS = 100;
+    public static final int INITBALANCE = 1000;
+
+    public static void main(String[] args) {
+        Bank bank = new Bank(NNACOUNTS, INITBALANCE);
+        for (int index = 0; index < NNACOUNTS; index++) {
+            new Thread(new TransferRunnable(bank, index, INITBALANCE)).start();
+        }
+    }
+}
