04243d568b5cce092f0d365c3b07a24ffcbcc5b2
clx <brianchen.clx@gmail.com>
2017-03-22 22:31:17 +0800

    thread safe transfer

--- Java-core/src/main/java/com/java/core/thread/bank/synch/Bank.java
+++ Java-core/src/main/java/com/java/core/thread/bank/synch/Bank.java
@@ -8,48 +8,58 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class Bank {
 
-    private ReentrantLock bankLock = new ReentrantLock();
-    private final double[] accounts;
-    private Condition sufficientFunds;
+	private ReentrantLock bankLock;
+	private final double[] accounts;
+	private Condition sufficientFunds;
 
-    public Bank(int n, int initBalance) {
-        accounts = new double[n];
-        for (int index = 0; index < accounts.length; index++) {
-            accounts[index] = initBalance;
-        }
-        sufficientFunds = bankLock.newCondition();
-    }
+	public Bank(int n, int initBalance) {
+		accounts = new double[n];
+		for (int index = 0; index < accounts.length; index++) {
+			accounts[index] = initBalance;
+		}
+		bankLock = new ReentrantLock();
+		sufficientFunds = bankLock.newCondition();
+	}
 
-    public void unsynchTransfer(int from, int to, double amount) {
-        System.out.print(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf(" %10.2f from %d to %d", amount, from, to);
-        accounts[to] += amount;
-        System.out.println("Total balance:" + getTotalBalance());
-    }
+	public void unsynchTransfer(int from, int to, double amount) {
+		System.out.print(Thread.currentThread());
+		accounts[from] -= amount;
+		System.out.printf(" %10.2f from %d to %d", amount, from, to);
+		accounts[to] += amount;
+		System.out.println("Total balance:" + getTotalBalance());
+	}
 
-    public void synchTransfer(int from, int to, double amount) {
-        bankLock.lock();
-        try {
-            System.out.print(Thread.currentThread());
-            accounts[from] -= amount;
-            System.out.printf(" %10.2f from %d to %d", amount, from, to);
-            accounts[to] += amount;
-            System.out.println("Total balance:" + getTotalBalance());
-        } finally {
-            bankLock.unlock();
-        }
-    }
+	public void synchTransfer(int from, int to, double amount) throws InterruptedException {
+		bankLock.lock();
+		try {
+			while (accounts[from] < amount) {
+				sufficientFunds.await();
+			}
+			System.out.print(Thread.currentThread());
+			accounts[from] -= amount;
+			System.out.printf(" %10.2f from %d to %d", amount, from, to);
+			accounts[to] += amount;
+			System.out.println("Total balance:" + getTotalBalance());
+			sufficientFunds.signalAll();
+		} finally {
+			bankLock.unlock();
+		}
+	}
 
-    public double getTotalBalance() {
-        double sum = 0;
-        for (double a : accounts) {
-            sum += a;
-        }
-        return sum;
-    }
+	public double getTotalBalance() {
+		bankLock.lock();
+		try {
+			double sum = 0;
+			for (double a : accounts) {
+				sum += a;
+			}
+			return sum;
+		} finally {
+			bankLock.unlock();
+		}
+	}
 
-    public int size() {
-        return accounts.length;
-    }
+	public int size() {
+		return accounts.length;
+	}
 }
