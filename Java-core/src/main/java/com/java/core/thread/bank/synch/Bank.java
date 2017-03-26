c81392be6251a8a423fb88df0fe3237964de9619
clx <brianchen.clx@gmail.com>
2017-03-26 23:45:41 +0800

    sync block

--- Java-core/src/main/java/com/java/core/thread/bank/synch/Bank.java
+++ Java-core/src/main/java/com/java/core/thread/bank/synch/Bank.java
@@ -72,6 +72,23 @@ public class Bank {
 		notifyAll();
 	}
 
+	/**
+	 * 同步阻塞
+	 * @param from
+	 * @param to
+	 * @param amount
+	 * @throws InterruptedException
+	 */
+	public void transferSyncBlock(int from, int to, double amount) throws InterruptedException {
+		synchronized (this) {
+			System.out.print(Thread.currentThread());
+			accounts[from] -= amount;
+			System.out.printf(" %10.2f from %d to %d", amount, from, to);
+			accounts[to] += amount;
+			System.out.println("Total balance:" + getTotalBalance());
+		}
+	}
+
 	public synchronized double getTotalBalance() {
 		double sum = 0;
 		for (double a : accounts) {
