cfd2a2377d5607fd9168efacc5db4fc5dadf3918
brianchen.clx <brianchen.clx@gmail.com>
2017-04-06 17:39:29 +0800

    dead lock test

--- Java-core/src/main/java/com/java/core/thread/bank/synch/SynchTransferTest.java
+++ Java-core/src/main/java/com/java/core/thread/bank/synch/SynchTransferTest.java
@@ -3,9 +3,9 @@ package com.java.core.thread.bank.synch;
 /**
  * @author chenlixin at 2017年3月6日 下午6:21:17
  */
-public class UnsynchTransferTest {
+public class SynchTransferTest {
 
-    public static final int NNACOUNTS = 100;
+    public static final int NNACOUNTS = 5;
     public static final int INITBALANCE = 1000;
 
     public static void main(String[] args) {
