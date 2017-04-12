309a6b7107ca5c634e4a0d5aa7ed71660fde8f6d
clx <brianchen.clx@gmail.com>
2017-04-12 23:31:30 +0800

    init future task

---
+++ Java-core/src/main/java/com/java/core/thread/futuretask/FutureTest.java
@@ -0,0 +1,11 @@
+package com.java.core.thread.futuretask;
+
+/**
+ * @author clx 2017年4月12日 下午11:27:41
+ */
+public class FutureTest {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Java-core/src/main/java/com/java/core/thread/futuretask/MatchCounter.java
@@ -0,0 +1,13 @@
+package com.java.core.thread.futuretask;
+
+import java.io.File;
+
+/**
+ * @author clx 2017年4月12日 下午11:28:17
+ */
+public class MatchCounter {
+
+	private File directory;
+	private String keyWord;
+	private int count;
+}
