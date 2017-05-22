194f0b984c269c6b81a87ac6f5604209bb541600
brianchen.clx <brianchen.clx@gmail.com>
2017-05-22 16:18:02 +0800

    fix fizzbuzz

--- Java-core/src/main/java/com/java/core/basic/FizzBuzz.java
+++ Java-core/src/main/java/com/java/core/basic/FizzBuzz.java
@@ -6,11 +6,31 @@ package com.java.core.basic;
 public class FizzBuzz {
 
     public static void main(String[] args) {
+        FizzBuzz.fizzBuzzVerSecond(100);
+    }
+
+    public static void fizzBuzzVerFirst() {
         for (int idx = 1; idx <= 100; idx++) {
             if (idx % 3 == 0 && idx % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (idx % 3 == 0) {
                 System.out.println("Fizz");
+            } else if (idx % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(idx);
+            }
+        }
+    }
+
+    public static void fizzBuzzVerSecond(int n) {
+        for (int idx = 1; idx <= n; idx++) {
+            if (idx % 3 == 0) {
+                if (idx % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
             } else if (idx % 5 == 0) {
                 System.out.println("Buzz");
             } else {
