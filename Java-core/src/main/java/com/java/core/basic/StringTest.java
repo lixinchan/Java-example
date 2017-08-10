e5526f23d100c70137d6dcad8c364c36ebbe362b
brianchen.clx <brianchen.clx@gmail.com>
2017-08-10 19:24:15 +0800

    string test

--- Java-core/src/main/java/com/java/core/basic/StringTest.java
+++ Java-core/src/main/java/com/java/core/basic/StringTest.java
@@ -10,9 +10,21 @@ public class StringTest {
         // System.out.println("false");
         // }
 
-        for (int i = 10; i > 0; --i)
-            System.out.println("Counting down..." + i);
-        System.out.println("Time out!");
+        // for (int i = 10; i > 0; --i)
+        // System.out.println("Counting down..." + i);
+        // System.out.println("Time out!");
+
+        long timeStamp = System.currentTimeMillis();
+        long time = timeStamp / 1000;
+        System.out.println(time);
+        // System.out.println((time << 20) | (1 << 8));
+        System.out.println(Long.toBinaryString(time).length());
+        System.out.println(Long.toBinaryString(time << 28).length());
+        System.out.println(Long.toBinaryString(1 << 8));
+        System.out.println(Long
+                .toBinaryString((time << 28) | (262143 << 10) | 9).length());
+        // System.out.println(Long.toBinaryString(262143 << 10).length());
+        // System.out.println(Long.toBinaryString(9).length());
     }
 
 }
