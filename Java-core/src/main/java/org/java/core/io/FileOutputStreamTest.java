937438015de0643cd387aaea491b65d83d06218c
clx <brianchen.clx@gmail.com>
2017-11-14 07:48:35 +0800

    stream test

---
+++ Java-core/src/main/java/org/java/core/io/FileOutputStreamTest.java
@@ -0,0 +1,25 @@
+package org.java.core.io;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+/**
+ * @author clx 2017/11/14 7:20
+ */
+public class FileOutputStreamTest {
+
+	public static void main(String[] args) throws Exception {
+		execTest();
+	}
+
+	private static void execTest() throws Exception {
+		File file = new File("E:" + File.separator + "test" + File.separator + "stream.txt");
+		OutputStream out = new FileOutputStream(file);
+		byte[] bytes = "HelloWorld.".getBytes();
+		out.write(bytes);
+		out.close();
+	}
+
+
+}
