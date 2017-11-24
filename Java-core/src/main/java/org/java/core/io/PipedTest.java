b8d22f139c1280324698973cdc84e3aa4b6188e3
clx <brianchen.clx@gmail.com>
2017-11-24 09:22:16 +0800

    Piped stream test

---
+++ Java-core/src/main/java/org/java/core/io/PipedTest.java
@@ -0,0 +1,27 @@
+package org.java.core.io;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+/**
+ * @author clx 2017/11/23 23:54
+ */
+public class PipedTest {
+
+	public static void main(String[] args) {
+		PipedOutputStreamTest out = new PipedOutputStreamTest();
+		PipedInputStreamTest in = new PipedInputStreamTest();
+		Thread outThread = new Thread(out);
+		Thread inThread = new Thread(in);
+		PipedOutputStream outputStream = out.getOutputStream();
+		PipedInputStream inputStream = in.getInputStream();
+		try {
+			outputStream.connect(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		outThread.start();
+		inThread.start();
+	}
+}
