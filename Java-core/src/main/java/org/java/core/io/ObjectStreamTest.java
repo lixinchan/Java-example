ff1156f8b500aacd35cdfc9fb78e9c8946cda392
clx <brianchen.clx@gmail.com>
2017-11-27 00:06:12 +0800

    Object stream test

---
+++ Java-core/src/main/java/org/java/core/io/ObjectStreamTest.java
@@ -0,0 +1,50 @@
+package org.java.core.io;
+
+import java.io.*;
+
+/**
+ * @author clx 2017/11/26 23:53
+ */
+public class ObjectStreamTest {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("E:" + File.separator + "test" + File.separator + "serializable.txt");
+		serializable(file);
+		deserializable(file);
+	}
+
+	public static void serializable(File file) throws Exception {
+		OutputStream out = new FileOutputStream(file);
+		ObjectOutputStream oos = new ObjectOutputStream(out);
+		oos.writeObject(new Person("clx", 27));
+		oos.close();
+	}
+
+	public static void deserializable(File file) throws Exception {
+		InputStream in = new FileInputStream(file);
+		ObjectInputStream ois = new ObjectInputStream(in);
+		Person person = (Person) ois.readObject();
+		System.out.println(person);
+	}
+
+}
+
+class Person implements Serializable {
+
+	private transient String name;
+	private int age;
+	private final static String GENDER = "man";
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				'}';
+	}
+}
