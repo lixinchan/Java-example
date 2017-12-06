0e849fcf5c4d6445786c3ff52c165d2801fdafe1
clx <brianchen.clx@gmail.com>
2017-12-06 18:16:09 +0800

    optimize code

--- Java-core/src/main/java/org/java/draw/Gift.java
+++ Java-core/src/main/java/org/java/draw/Gift.java
@@ -4,53 +4,65 @@ package org.java.draw;
  * @author chenlixin at 2015年9月14日 下午3:38:01
  */
 public class Gift {
-    private int index;
-    private String gitfId;
-    private String giftName;
-    private double probability;
+	/**
+	 * id
+	 */
+	private int index;
+	/**
+	 * gift id
+	 */
+	private String giftId;
+	/**
+	 * gift name
+	 */
+	private String giftName;
+	/**
+	 * 中奖几率
+	 */
+	private double probability;
 
-    public Gift(int index, String gitfId, String giftName, double probability) {
-        this.index = index;
-        this.gitfId = gitfId;
-        this.giftName = giftName;
-        this.probability = probability;
-    }
+	public Gift(int index, String giftId, String giftName, double probability) {
+		this.index = index;
+		this.giftId = giftId;
+		this.giftName = giftName;
+		this.probability = probability;
+	}
 
-    public int getIndex() {
-        return index;
-    }
+	public int getIndex() {
+		return index;
+	}
 
-    public void setIndex(int index) {
-        this.index = index;
-    }
+	public void setIndex(int index) {
+		this.index = index;
+	}
 
-    public String getGitfId() {
-        return gitfId;
-    }
+	public String getGiftId() {
+		return giftId;
+	}
 
-    public void setGitfId(String gitfId) {
-        this.gitfId = gitfId;
-    }
+	public void setGiftId(String giftId) {
+		this.giftId = giftId;
+	}
 
-    public String getGiftName() {
-        return giftName;
-    }
+	public String getGiftName() {
+		return giftName;
+	}
 
-    public void setGiftName(String giftName) {
-        this.giftName = giftName;
-    }
+	public void setGiftName(String giftName) {
+		this.giftName = giftName;
+	}
 
-    public double getProbability() {
-        return probability;
-    }
+	public double getProbability() {
+		return probability;
+	}
 
-    public void setProbability(double probability) {
-        this.probability = probability;
-    }
+	public void setProbability(double probability) {
+		this.probability = probability;
+	}
 
-    @Override
-    public String toString() {
-        return "Gift [index=" + index + ", gitfId=" + gitfId + ", giftName="
-                + giftName + ", probability=" + probability + "]";
-    }
+	@Override
+	public String toString() {
+		return "Gift [index=" + index + ", giftId=" + giftId + ", giftName="
+				+ giftName + ", probability=" + probability + "]";
+	}
 }
