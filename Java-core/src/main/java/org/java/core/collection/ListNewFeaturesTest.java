18035d82898f026a58a2bcc9719aecd37a6bc611
clx <brianchen.clx@gmail.com>
2018-03-10 23:57:10 +0800

    add filter test

--- Java-core/src/main/java/org/java/core/collection/ListNewFeaturesTest.java
+++ Java-core/src/main/java/org/java/core/collection/ListNewFeaturesTest.java
@@ -11,41 +11,46 @@ import java.util.stream.Collectors;
  */
 public class ListNewFeaturesTest {
 
-	private static List<String> list;
+    private static List<String> list;
 
-	/**
-	 * return list
-	 *
-	 * @return
-	 */
-	public static List<String> getList() {
-		return list;
-	}
+    /**
+     * return list
+     *
+     * @return
+     */
+    public static List<String> getList() {
+        return list;
+    }
 
-	/**
-	 * init list
-	 */
-	static {
-		list = new ArrayList<>();
-		list.add("a");
-		list.add("b");
-		list.add("c");
-		list.add("d");
-	}
+    /**
+     * init list
+     */
+    static {
+        list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+    }
 
-	/**
-	 * filter test
-	 *
-	 * @param list
-	 * @param target
-	 * @return
-	 */
-	private static List<String> filterTest(List<String> list, final String target) {
-		return list.stream().filter(key -> !key.equals(target)).collect(Collectors.<String>toList());
-	}
+    /**
+     * filter test
+     *
+     * @param list
+     * @param target
+     * @return
+     */
+    private static List<String> filterNotEquals(List<String> list, final String target) {
+        return list.stream().filter(key -> !key.equals(target)).collect(Collectors.toList());
+    }
 
-	public static void main(String[] args) {
-		String target = "a";
-		filterTest(getList(), target).forEach(System.out::println);
-	}
+    private static List<String> filterEquals(List<String> list, String target) {
+        return list.stream().filter(key -> key.equals(target)).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        String target = "a";
+        filterNotEquals(getList(), target).forEach(System.out::println);
+        filterEquals(getList(), target).forEach(System.out::println);
+    }
 }
