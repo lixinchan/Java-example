be985dadb8e024b764ffb6ab97283e4df8464a55
clx <brianchen.clx@gmail.com>
2018-04-02 18:04:38 +0800

    add volatile test

---
+++ Java-core/src/main/java/org/effective/java/chapter/tenth/StopThread.java
@@ -0,0 +1,33 @@
+package org.effective.java.chapter.tenth;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author clx 2018/4/2.
+ */
+public class StopThread {
+	private volatile boolean stopRequested;
+
+	public static void main(String[] args) {
+		try {
+			new StopThread().test();
+		} catch (Exception ex) {
+			ex.getMessage();
+		}
+	}
+
+	private void test() throws InterruptedException {
+		Thread backgroundThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int i = 0;
+				while (!stopRequested) {
+					i++;
+				}
+			}
+		});
+		backgroundThread.start();
+		TimeUnit.SECONDS.sleep(1);
+		stopRequested = true;
+	}
+}
