b030cd8ee6da2ed47a8e168207aeae0f3929f9f5
clx <brianchen.clx@gmail.com>
2018-04-08 15:08:26 +0800

    add member inner class

---
+++ Java-core/src/test/java/org/core/java/innerclass/TestMemberInnerClass.java
@@ -0,0 +1,14 @@
+package org.core.java.innerclass;
+
+/**
+ * @author clx 2018/4/8.
+ */
+public class TestMemberInnerClass {
+
+	public static void main(String[] args) {
+		MemberInnerClass outer = new MemberInnerClass(1);
+		outer.privateInnerGetIdx();
+		MemberInnerClass.PublicInner publicInner = outer.new PublicInner();
+		publicInner.printIdx();
+	}
+}
