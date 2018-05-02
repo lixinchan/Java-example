1fd98b050f6fbc9de5726cc0c5bbfeceae6b35cf
clx <brianchen.clx@gmail.com>
2018-05-02 15:20:54 +0800

    test oom

--- Java-core/src/test/java/org/demo/core/test/Test.java
+++ Java-core/src/test/java/org/demo/core/test/Test.java
@@ -1,8 +1,11 @@
 package org.demo.core.test;
 
-import java.util.*;
-
 import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author clx 2017年11月11日 下午7:48:36
@@ -10,7 +13,7 @@ import static java.util.stream.Collectors.toList;
 public class Test {
 
 	public static void main(String[] args) {
-		Double[] array = new Double[]{1.2, 3.1, 2.3, 0.2};
+		Double[] array = new Double[] { 1.2, 3.1, 2.3, 0.2 };
 		List<Double> sortedVals = new ArrayList<>(Arrays.asList(array)).stream().filter(Objects::nonNull).sorted()
 				.collect(toList());
 		sortedVals.forEach(System.out::println);
