34a6a680d5e177ec64525a76157be80fcad321e6
clx <brianchen.clx@gmail.com>
2018-05-22 10:49:35 +0800

    ThreadPoolExecutor test

--- Java-core/src/main/java/org/core/java/thread/futuretask/FutureTest.java
+++ Java-core/src/main/java/org/core/java/thread/futuretask/FutureTest.java
@@ -1,9 +1,10 @@
 package org.core.java.thread.futuretask;
 
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+
 import java.io.File;
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 /**
  * @author clx 2017年4月12日 下午11:27:41
@@ -18,8 +19,15 @@ public class FutureTest {
 		String keyWord = in.nextLine();
 
 		MatchCounter counter = new MatchCounter(new File(directory), keyWord);
-		FutureTask<Integer> task = new FutureTask<Integer>(counter);
-		new Thread(task).start();
+		FutureTask<Integer> task = new FutureTask<>(counter);
+
+		int minThread = 2;
+		int maxThread = 2 * Runtime.getRuntime().availableProcessors() + 1;
+		ThreadFactory threadFactory = new BasicThreadFactory.Builder().namingPattern("future-pool-%d").build();
+		ExecutorService pool = new ThreadPoolExecutor(minThread, maxThread, 0L, TimeUnit.MILLISECONDS,
+				new LinkedBlockingQueue<>(1024), threadFactory, new ThreadPoolExecutor.AbortPolicy());
+		pool.execute(task);
+		pool.shutdown();
 
 		try {
 			System.out.println(task.get() + " matching files.");
