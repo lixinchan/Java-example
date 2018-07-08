a97ce7f87fd210d7cc3a5c4a4304345a18c72b1d
clx <brianchen.clx@gmail.com>
2018-07-08 22:25:04 +0800

    init design patterns

---
+++ src/main/java/org/design/patterns/pattern/AbstractFactory.java
@@ -0,0 +1,7 @@
+package org.design.patterns.pattern;
+
+/**
+ * @author clx 2018/7/8
+ */
+public class AbstractFactory {
+}
---
+++ src/main/java/org/design/patterns/principle/SingleResponsibility.java
@@ -0,0 +1,9 @@
+package org.design.patterns.principle;
+
+/**
+ * SRP
+ * 
+ * @author clx 2018/7/8
+ */
+public class SingleResponsibility {
+}
