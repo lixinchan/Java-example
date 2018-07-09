fd42146d10cba78b0144d7bd305fa48d0fd00209
clx <brianchen.clx@gmail.com>
2018-07-09 09:21:03 +0800

    SRP

--- src/main/java/org/design/patterns/principle/SingleResponsibility.java
+++ src/main/java/org/design/patterns/principle/SingleResponsibility.java
@@ -1,9 +1,86 @@
 package org.design.patterns.principle;
 
+import java.sql.Connection;
+import java.util.List;
+
 /**
- * SRP
+ * SRP,一个类只负责相应功能领域的职责
  * 
  * @author clx 2018/7/8
  */
 public class SingleResponsibility {
+
+}
+
+/**
+ * after refactor
+ */
+interface consumerDataChart {
+
+	/**
+	 * create customer chart
+	 */
+	void createChart();
+
+	/**
+	 * display customer chart
+	 */
+	void showChart();
+}
+
+/**
+ * after refactor
+ */
+interface CustomerDao {
+	/**
+	 * get customers
+	 *
+	 * @return
+	 */
+	List getCustomers();
+}
+
+/**
+ * after refactor
+ */
+class DBUtils {
+
+	/**
+	 * get db connection
+	 * 
+	 * @return
+	 */
+	Connection getConnection() {
+		return null;
+	}
+}
+
+/**
+ * before refactor, the response of class CustomerDataChart
+ */
+interface CustomerDataChart {
+
+	/**
+	 * get db connection
+	 * 
+	 * @return
+	 */
+	Connection getConnection();
+
+	/**
+	 * get customers
+	 * 
+	 * @return
+	 */
+	List getCustomers();
+
+	/**
+	 * create customer chart
+	 */
+	void createChart();
+
+	/**
+	 * display customer chart
+	 */
+	void showChart();
 }
