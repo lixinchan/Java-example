cb7f6c18021cdfe64aee4f58113864f3b1411be6
clx <brianchen.clx@gmail.com>
2018-07-23 23:58:15 +0800

    add ISP

---
+++ src/main/java/org/design/patterns/principle/InterfaceSegregationPrinciple.java
@@ -0,0 +1,10 @@
+package org.design.patterns.principle;
+
+/**
+ * ISP,使用多个专门的接口，而不使用单一的总接口，即客户端不应该依赖那些它不需要的接口
+ * 
+ * @author clx 2018/7/23
+ */
+public class InterfaceSegregationPrinciple {
+	
+}
