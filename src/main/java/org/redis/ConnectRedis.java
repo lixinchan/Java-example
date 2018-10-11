6330cbfe7163c5c07390a9bd9348896ae1fba295
clx <brianchen.clx@gmail.com>
2018-10-11 18:05:58 +0800

    init redis test

---
+++ src/main/java/org/redis/ConnectRedis.java
@@ -0,0 +1,16 @@
+package org.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * @author clx 2018/10/11.
+ */
+public class ConnectRedis {
+
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("192.168.11.130", 6379);
+		jedis.auth("clx");
+		System.out.println(jedis.ping());
+		System.out.println(jedis.get("test"));
+	}
+}
