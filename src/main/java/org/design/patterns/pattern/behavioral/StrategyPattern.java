d77a123a78e060228c30e58a66abdfcf3c615832
clx <brianchen.clx@gmail.com>
2018-10-18 21:50:34 +0800

    init pattern

---
+++ src/main/java/org/design/patterns/pattern/behavioral/StrategyPattern.java
@@ -0,0 +1,7 @@
+package org.design.patterns.pattern.behavioral;
+
+/**
+ * @author clx 2018/10/15.
+ */
+public class StrategyPattern {
+}
---
+++ src/main/java/org/design/patterns/pattern/structural/AdapterPattern.java
@@ -0,0 +1,8 @@
+package org.design.patterns.pattern.structural;
+
+/**
+ * @author clx 2018/10/15.
+ */
+public class AdapterPattern {
+
+}
