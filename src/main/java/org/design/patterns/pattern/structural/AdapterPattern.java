7c19e51c3d90f892d92a35249052c5c5f603931e
clx <brianchen.clx@gmail.com>
2018-11-02 21:45:36 +0800

    add adapter pattern

--- src/main/java/org/design/patterns/pattern/structural/AdapterPattern.java
+++ src/main/java/org/design/patterns/pattern/structural/AdapterPattern.java
@@ -1,8 +1,96 @@
 package org.design.patterns.pattern.structural;
 
 /**
+ * adapter pattern
+ * 
  * @author clx 2018/10/15.
  */
 public class AdapterPattern {
 
+	public static void main(String[] args) {
+
+	}
+}
+
+/**
+ * target class
+ */
+interface TargetClass {
+
+	/**
+	 * first
+	 */
+	void getFirst();
+
+	/**
+	 * second
+	 */
+	void getSecond();
+}
+
+/**
+ * source class
+ */
+class SourceClass {
+
+	/**
+	 * first
+	 */
+	public void getFirst() {
+
+	}
+
+}
+
+/**
+ * class adapter
+ */
+class ClassAdapter extends SourceClass implements TargetClass {
+
+	@Override
+	public void getSecond() {
+		System.out.println("adapter second.");
+	}
+}
+
+/**
+ * object source class
+ */
+class ObjectSource {
+
+	/**
+	 * first
+	 */
+	public void getFirst() {
+
+	}
+}
+
+/**
+ * object adapter
+ */
+class ObjectAdapter {
+
+	/**
+	 * source
+	 */
+	private ObjectSource source;
+
+	public ObjectAdapter(ObjectSource source) {
+		this.source = source;
+	}
+
+	/**
+	 * first
+	 */
+	public void getFirst() {
+		this.source.getFirst();
+	}
+
+	/**
+	 * second
+	 */
+	public void getSecond() {
+
+	}
 }
