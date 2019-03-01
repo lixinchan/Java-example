162a5559a6ba06d1d93300909c1632f93fbd25b0
clx <brianchen.clx@gmail.com>
2019-03-01 13:26:34 +0800

    reflect test

--- src/main/java/org/example/core/java/reflect/ReflectTest.java
+++ src/main/java/org/example/core/java/reflect/ReflectTest.java
@@ -1,93 +1,108 @@
 package org.example.core.java.reflect;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 
+/**
+ * reflect
+ * 
+ * @author clx
+ */
 public class ReflectTest {
 
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class clazz = Class.forName("java.lang.String");
-        ReflectTest.printConstruct(clazz);
-        System.out.println("-------------------------");
-        ReflectTest.printFields(clazz);
-        System.out.println("-------------------------");
-        ReflectTest.printMethods(clazz);
-    }
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class clazz = Class.forName("java.lang.String");
+		System.out.println("-----------Construct-----------");
+		ReflectTest.printConstruct(clazz);
+		System.out.println("-----------fields--------------");
+		ReflectTest.printFields(clazz);
+		System.out.println("-----------methods-------------");
+		ReflectTest.printMethods(clazz);
+	}
 
-    public static void printFields(Class clazz) {
-        Field[] fields = clazz.getDeclaredFields();
+	/**
+	 * fields
+	 * 
+	 * @param clazz
+	 */
+	private static void printFields(Class clazz) {
+		Field[] fields = clazz.getDeclaredFields();
+		if (fields.length == 0) {
+			return;
+		}
+		for (Field field : fields) {
+			String fieldName = field.getName();
+			Class fieldType = field.getType();
+			String modifier = Modifier.toString(field.getModifiers());
+			if (modifier.length() > 0) {
+				System.out.print(modifier + " ");
+			}
+			System.out.println(fieldType.getName() + " " + fieldName);
+		}
+	}
 
-        for (Field f : fields) {
-            // field name
-            String name = f.getName();
+	/**
+	 * construct
+	 * 
+	 * @param clazz
+	 */
+	private static void printConstruct(Class clazz) {
+		Constructor[] constructors = clazz.getDeclaredConstructors();
+		if (constructors.length == 0) {
+			return;
+		}
+		for (Constructor constructor : constructors) {
+			String modifier = Modifier.toString(constructor.getModifiers());
+			if (modifier.length() > 0) {
+				System.out.print(modifier + " ");
+			}
+			System.out.print(constructor.getName());
+			Class[] types = constructor.getParameterTypes();
+			if (types.length == 0) {
+				System.out.println("();");
+				continue;
+			}
+			System.out.print("(");
+			Parameter[] parameters = constructor.getParameters();
+			for (int idx = 0; idx < types.length; idx++) {
+				if (idx > 0) {
+					System.out.print(", ");
+				}
+				System.out.print(types[idx].getName() + " " + parameters[idx].getName());
+			}
+			System.out.println(");");
+		}
+	}
 
-            // modifier
-            String modifiers = Modifier.toString(f.getModifiers());
-
-            // params type
-            Class type = f.getType();
-
-            if (modifiers.length() > 0)
-                System.out.print(modifiers + " ");
-            System.out.print(type.getName() + " " + name);
-            System.out.println(";");
-        }
-    }
-
-    public static void printConstruct(Class clazz) {
-        Constructor[] cons = clazz.getDeclaredConstructors();
-
-        for (Constructor c : cons) {
-            // constructor name
-            String name = c.getName();
-
-            // modifier
-            String modifiers = Modifier.toString(c.getModifiers());
-
-            if (modifiers.length() > 0)
-                System.out.print(modifiers + " ");
-            System.out.print(name + "(");
-
-            // print param type
-            Class[] paramTypes = c.getParameterTypes();
-
-            for (int i = 0; i < paramTypes.length; ++i) {
-                if (i > 0)
-                    System.out.print(", ");
-                System.out.print(paramTypes[i].getName());
-            }
-            System.out.println(");");
-        }
-    }
-
-    public static void printMethods(Class clazz) {
-        Method[] methods = clazz.getDeclaredMethods();
-
-        for (Method method : methods) {
-            // method name
-            String name = method.getName();
-
-            // modifiers
-            String modifiers = Modifier.toString(method.getModifiers());
-            if (modifiers.length() > 0)
-                System.out.print(modifiers + " ");
-
-            // return type
-            Class retType = method.getReturnType();
-
-            System.out.print(retType.getName() + " ");
-            System.out.print(name + "(");
-
-            // params type
-            Class[] paramTypes = method.getParameterTypes();
-            for (int i = 0; i < paramTypes.length; ++i) {
-                if (i > 0)
-                    System.out.print(", ");
-                System.out.print(paramTypes[i].getName());
-            }
-            System.out.println(");");
-        }
-    }
+	/**
+	 * methods
+	 * 
+	 * @param clazz
+	 */
+	private static void printMethods(Class clazz) {
+		Method[] methods = clazz.getDeclaredMethods();
+		if (methods.length == 0) {
+			return;
+		}
+		for (Method method : methods) {
+			String modifier = Modifier.toString(method.getModifiers());
+			if (modifier.length() > 0) {
+				System.out.print(modifier + " ");
+			}
+			System.out.print(method.getName());
+			Class[] types = method.getParameterTypes();
+			if (types.length == 0) {
+				System.out.println("();");
+				continue;
+			}
+			System.out.print("(");
+			Parameter[] parameters = method.getParameters();
+			for (int idx = 0; idx < types.length; idx++) {
+				if (idx > 0) {
+					System.out.print(", ");
+				}
+				System.out.print(types[idx].getName() + " " + parameters[idx].getName());
+			}
+			System.out.println(");");
+		}
+	}
 }
