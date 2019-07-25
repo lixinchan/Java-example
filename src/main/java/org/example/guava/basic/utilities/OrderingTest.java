9b8f1a1064114ad70e990c1c008dc4a3d502ea7a
clx <brianchen.clx@gmail.com>
2019-07-25 14:17:55 +0800

    utilities test

---
+++ src/main/java/org/example/guava/basic/utilities/OrderingTest.java
@@ -0,0 +1,15 @@
+package org.example.guava.basic.utilities;
+
+/**
+ * @author clx 2019-07-17
+ */
+public class OrderingTest {
+
+	public static void main(String[] args) {
+		test();
+	}
+
+	private static void test() {
+
+	}
+}
