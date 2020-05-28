8af0b5cb6a050b36cbb982d35d44f45523d40923
clx <brianchen.clx@gmail.com>
2020-05-28 22:19:05 +0800

    add:ThreadStatus

---
+++ src/main/java/com/example/core/java/thread/TestThreadStatus.java
@@ -0,0 +1,47 @@
+package com.example.core.java.thread;
+
+/**
+ * @author clx
+ */
+public class TestThreadStatus {
+
+	public static void main(String[] args) {
+		try {
+			new TestThreadStatus().mainThread();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void mainThread() throws InterruptedException {
+		Thread a = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				testMethod();
+			}
+		}, "a");
+
+		Thread b = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				testMethod();
+			}
+		}, "b");
+
+		a.start();
+//		Thread.sleep(1000L); // thread b blocked
+//		a.join(); // thread b waiting
+		a.join(1000L);
+		b.start();
+		System.out.println(a.getName() + "->" + a.getState());
+		System.out.println(b.getName() + "->" + b.getState());
+	}
+
+	private synchronized void testMethod() {
+		try {
+			Thread.sleep(2000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
