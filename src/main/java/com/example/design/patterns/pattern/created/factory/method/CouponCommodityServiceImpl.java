5afbfd371fd22625fbe4411c2044a97d33731c95
clx <lixinchan.clx@gmail.com>
2022-01-08 21:51:23 +0800

    add:add factory pattern

---
+++ src/main/java/com/example/design/patterns/pattern/created/factory/method/CouponCommodityServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.design.patterns.pattern.created.factory.method;
+
+/**
+ * coupon commodity
+ *
+ * @author clx
+ */
+public class CouponCommodityServiceImpl implements CommodityService {
+
+	@Override
+	public void lotteryDraw(String userId, String lotteryId, Integer bizType) {
+		
+	}
+}
---
+++ src/main/java/com/example/design/patterns/pattern/created/factory/method/StoreFactoryImpl.java
@@ -0,0 +1,9 @@
+package com.example.design.patterns.pattern.created.factory.method;
+
+/**
+ * 工厂实现
+ *
+ * @author clx
+ */
+public class StoreFactoryImpl implements StoreFactory {
+}
